328d9b4d0cd6e233201746ec084c44a3df667bdd
jiaoziqun <274104498@qq.com>
2026-06-21 01:05:08 +0800

    新增代码格式审查，本地拦截以及Action远程审查

--- git_action_test.py
+++ git_action_test.py
@@ -1,24 +1,24 @@
 class calculate:
+    # 此处只在类被定义时才执行(一次)
 
-    #此处只在类被定义时才执行(一次)
-
-    def __init__(self,a,b):#此处在类实例化时执行(每次)
+    def __init__(self, a, b):  # 此处在类实例化时执行(每次)
         print("计算类实例化")
         self.a = a
         self.b = b
-    
+
     def Add(self):
         return self.a + self.b
-    
+
     def Sub(self):
         return self.a - self.b
-    
+
     def Mul(self):
         return self.a * self.b
-    
+
     def Div(self):
         return self.a / self.b
-    
+
+
 def test_calculate(calculate_test):
     assert calculate_test.Add() == 15
     assert calculate_test.Sub() == 5
--- test_calculate.py
+++ test_calculate.py
@@ -1,22 +1,27 @@
-#pytest文件
+# pytest文件
 import pytest
 from git_action_test import calculate
+
 
 def test_add():
     calc = calculate(10, 5)
     assert calc.Add() == 15
 
+
 def test_sub():
     calc = calculate(10, 5)
     assert calc.Sub() == 5
+
 
 def test_mul():
     calc = calculate(10, 5)
     assert calc.Mul() == 50
 
+
 def test_div():
     calc = calculate(10, 5)
     assert calc.Div() == 2.0
+
 
 def test_div_by_zero():
     calc = calculate(10, 0)
